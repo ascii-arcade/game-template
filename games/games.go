38a37c091be4fe2671f4f00a0e4a80a51ce8bf56
Keith Thibodeaux <keith@railscoder.net>
2025-06-01 08:02:11 -0400

    rename for clarity

--- games/games.go
+++ games/games.go
@@ -30,7 +30,7 @@ func New() *Game {
 	return game
 }
 
-func Get(code string) (*Game, error) {
+func GetOpenGame(code string) (*Game, error) {
 	game, exists := games[code]
 	if !exists {
 		return nil, errors.New("game not found")
